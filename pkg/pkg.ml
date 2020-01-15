514ae2bd9b6adfe294e8b6badf1e1767b047ff02
Kartik Singhal <kartiksinghal@gmail.com>
2020-01-15 09:13:20 -0600

    Add code, switch to dune-release

--- pkg/pkg.ml
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env ocaml
-#use "topfind"
-#require "topkg"
-open Topkg
-
-let () =
-  Pkg.describe "openqasm-parser" @@ fun c ->
-  Ok [ Pkg.mllib "src/openqasm-parser.mllib";
-       Pkg.test "test/test"; ]
