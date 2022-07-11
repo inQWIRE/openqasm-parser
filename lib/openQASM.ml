a08e91035e8e3f4cc9310b714275bf760e522bf3
Kartik Singhal <kartiksinghal@gmail.com>
2022-07-11 08:52:27 -0500

    Revert "revised attempt"
    This reverts commit cc3e1b9dd8f015fccd2d10b8ff9e195a27dd3ece.

--- lib/openQASM.ml
+++ lib/openQASM.ml
@@ -7,19 +7,20 @@ let print_position outx lexbuf =
   fprintf outx "%s:%d:%d" pos.pos_fname
     pos.pos_lnum (pos.pos_cnum - pos.pos_bol + 1)
 
+let parse_with_error lexbuf =
+  try Parser.mainprogram Lexer.token lexbuf with
+  | Lexer.SyntaxError msg ->
+      fprintf stderr "%a: %s\n" print_position lexbuf msg;
+      []
+  | Parser.Error ->
+      fprintf stderr "%a: syntax error\n" print_position lexbuf;
+      exit (-1)
+
 (* core parsing routine *)
 let get_ast f =
   let ic = open_in f in
   let lexbuf = Lexing.from_channel ic in
-  let res = (try Parser.mainprogram Lexer.token lexbuf with
-            | Lexer.SyntaxError msg ->
-                fprintf stderr "%a: %s\n" print_position lexbuf msg;
-                []
-            | Parser.Error ->
-                fprintf stderr "%a: syntax error\n" print_position lexbuf;
-                close_in ic;
-                exit (-1)) in
   close_in ic;
-  res
+  parse_with_error lexbuf
 
 module AST = AST
